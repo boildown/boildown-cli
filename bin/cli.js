991ba4aed64be520d03f37fc2c4726245c3e9a3b
David Thomas Grübl <me@davidgruebl.com>
2015-12-08 21:15:19 +0200

    feat: adopt new format

--- bin/cli.js
+++ bin/cli.js
@@ -5,19 +5,21 @@
 const fs = require('fs')
 const path = require('path')
 
-const gentry = require('gentry')
+const boildown = require('boildown')
 const min = require('minimist')
 
-const wisdom = require('../lib/wisdom')
 const argv = min(process.argv.slice(2))
 const generatorName = argv._[0]
+
+const inquirer = require('inquirer')
+const packageQuestions = require('../lib/packageQuestions')
 
 if (!argv._.length) usage()
 doesGeneratorExist()
 
-const generator = require('gentry-' + generatorName)
-const questions = generator.scaffold.questions
-const actions = generator.scaffold.actions
+const generator = require('boildown-' + generatorName)
+const questions = generator.questions
+const actions = generator.actions
 
 if (argv._.length !== 1) {
   // existing project, pass commands to generator
@@ -26,31 +28,31 @@ if (argv._.length !== 1) {
   if (!generator.commands) process.exit(1)
   generator.commands(
     argv,
-    JSON.parse(fs.readFileSync(path.join(process.cwd(), 'gentry.json'), 'utf8'))
+    JSON.parse(fs.readFileSync(path.join(process.cwd(), 'boildown.json'), 'utf8'))
   )
   process.exit(0)
 }
 
-wisdom(questions, (answers) => {
+inquirer.prompt(packageQuestions.concat(questions), (answers) => {
   // only run the actions
   if (argv.actions) return runActions()
 
   // full scaffold
-  const dest = `${process.cwd()}/${answers.package.name}`
+  const dest = `${process.cwd()}/${answers.packageName}`
   console.log(`auto scaffold -\n${dest}`)
-  gentry.autoScaffold({
+  boildown.autoScaffold({
     answers,
     templateDir: generator.scaffold.templateDir,
     dest
   }, (err) => {
     if (err) throw err
-    const file = path.join(dest, 'gentry.json')
+    const file = path.join(dest, 'boildown.json')
     fs.writeFileSync(file, JSON.stringify(answers, null, 2))
     runActions()
   })
 
   function runActions () {
-    gentry.runActions({answers: answers, actions: actions}, (err) => {
+    boildown.runActions({answers, actions}, (err) => {
       if (err) console.error(err)
       process.exit(err ? 1 : 0)
     })
@@ -69,12 +71,12 @@ function usage () {
 
 function doesGeneratorExist () {
   try {
-    require.resolve(`gentry-${generatorName}`)
+    require.resolve(`boildown-${generatorName}`)
   } catch (err) {
     if (err.code === 'MODULE_NOT_FOUND') {
       console.log(`
       Generator ${generatorName} not installed, try:
-      $ npm install -g gentry-${generatorName}`)
+      $ npm install -g boildown-${generatorName}`)
       process.exit(1)
     }
     throw err
